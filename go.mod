a4ab69b74ff0bb75c9852e2c00835e8989711161
Anand Babu (AB) Periasamy <ab@minio.io>
2020-07-17 00:55:32 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/abperiasamy/blindfold
+
+go 1.14
+
+require (
+	gopkg.in/freeeve/uci.v1 v1.0.0-20160113194233-2365824b929d
+	launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+// +build go1.13
+
+/*
+ * blindfold (C) 2020 Anand Babu Periasamy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+	"gopkg.in/freeeve/uci.v1"
+	"log"
+)
+
+func main() {
+	eng, err := uci.NewEngine("/usr/games/stockfish")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set some engine options
+	eng.SetOptions(uci.Options{
+		Hash:    128,
+		Ponder:  false,
+		OwnBook: true,
+		MultiPV: 4,
+	})
+
+	// set the starting position
+	eng.SetFEN("rnb4r/ppp1k1pp/3bp3/1N3p2/1P2n3/P3BN2/2P1PPPP/R3KB1R b KQ - 4 11")
+
+	// set some result filter options
+	resultOpts := uci.HighestDepthOnly | uci.IncludeUpperbounds | uci.IncludeLowerbounds
+	results, _ := eng.GoDepth(10, resultOpts)
+
+	// print it (String() goes to pretty JSON for now)
+	fmt.Println(results)
+}
