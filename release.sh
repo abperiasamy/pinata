67607b0d50080ee932126ba576e5269ed579806f
Anand Babu (AB) Periasamy <twitter.com/abperiasamy>
2020-09-14 01:45:25 -0700

    update release latest version

--- release.sh
+++ release.sh
@@ -50,6 +50,8 @@ fi
 ## Update release string across the repo.
 sed -i 's/v'$OLD_VERSION'/v'$NEW_VERSION'/g' README.md
 sed -i 's/_'$OLD_VERSION'_/_'$NEW_VERSION'_/g' README.md
+sed -i 's/v'$OLD_VERSION'/v'$NEW_VERSION'/g' Dockerfile.release
+sed -i 's/_'$OLD_VERSION'_/_'$NEW_VERSION'_/g' Dockerfile.release
 sed -i 's/= "'$OLD_VERSION'"/= "'$NEW_VERSION'"/g' cmd/globals.go
 git commit -am  "Releasing version v"$NEW_VERSION
 git push
